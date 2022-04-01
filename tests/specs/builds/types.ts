058d3daab0e207d6c79235cf507264550215e6e8
Hiroki Osame <hiroki.osame@gmail.com>
2022-03-31 20:26:15 -0400

    test: cover multiple dts with same name

--- tests/specs/builds/types.ts
+++ tests/specs/builds/types.ts
@@ -65,5 +65,41 @@ export default testSuite(({ describe }, nodePath: string) => {
 
 			await fixture.cleanup();
 		});
+
+		test('emits multiple - same name', async () => {
+			const fixture = await createFixture('./tests/fixture-package');
+
+			await fixture.writeJson('package.json', {
+				exports: {
+					'./a': {
+						types: './dist/utils.d.ts',
+					},
+					'./b': {
+						types: './dist/nested/utils.d.ts',
+					},
+				},
+			});
+
+			await fixture.writeJson('tsconfig.json', {
+				compilerOptions: {
+					typeRoots: [
+						path.resolve('node_modules/@types'),
+					],
+				},
+			});
+
+			const pkgrollProcess = await pkgroll([], { cwd: fixture.path, nodePath });
+
+			expect(pkgrollProcess.exitCode).toBe(0);
+			expect(pkgrollProcess.stderr).toBe('');
+
+			const utilsDts = await fixture.readFile('dist/utils.d.ts', 'utf8');
+			expect(utilsDts).toMatch('declare function sayHello');
+
+			const nestedDts = await fixture.readFile('dist/nested/utils.d.ts', 'utf8');
+			expect(nestedDts).toMatch('declare function sayGoodbye');
+
+			await fixture.cleanup();
+		});
 	});
 });

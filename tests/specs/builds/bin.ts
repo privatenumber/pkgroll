a4ac701fde75b90bf9e2a5d2150a2bfa8b244aec
Hiroki Osame <hiroki.osame@gmail.com>
2022-04-20 16:38:31 -0400

    wip

--- tests/specs/builds/bin.ts
+++ tests/specs/builds/bin.ts
@@ -5,7 +5,6 @@ import { pkgroll } from '../../utils/pkgroll';
 
 export default testSuite(({ describe }, nodePath: string) => {
 	describe('bin', ({ test }) => {
-
 		(async () => {
 			const fixture = await createFixture('./tests/fixture-package');
 			await fixture.writeJson('package.json', {
@@ -14,7 +13,7 @@ export default testSuite(({ describe }, nodePath: string) => {
 
 			await test('supports single path', async () => {
 				const pkgrollProcess = await pkgroll([], { cwd: fixture.path, nodePath });
-	
+
 				expect(pkgrollProcess.exitCode).toBe(0);
 				expect(pkgrollProcess.stderr).toBe('');
 			});
@@ -23,12 +22,12 @@ export default testSuite(({ describe }, nodePath: string) => {
 				await test('is executable', async () => {
 					const content = await fixture.readFile('dist/index.mjs', 'utf8');
 					expect(content).toMatch('#!/usr/bin/env node');
-		
+
 					const stats = await fs.stat(`${fixture.path}/dist/index.mjs`);
 					const unixFilePermissions = `0${(stats.mode & 0o777).toString(8)}`; // eslint-disable-line no-bitwise
-		
+
 					expect(unixFilePermissions).toBe('0755');
-				});	
+				});
 			}
 
 			await fixture.cleanup();

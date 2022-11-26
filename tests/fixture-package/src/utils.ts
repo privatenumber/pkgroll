f9c746daad7e3b9c628db4890ad7f39c3253b46d
hiroki osame <hiroki.osame@gmail.com>
2022-11-26 10:35:38 -0500

    feat: support bundling `.d.ts` (#22)

--- tests/fixture-package/src/utils.ts
+++ tests/fixture-package/src/utils.ts
@@ -1,6 +1,7 @@
+import type { SomeType } from './types';
 export { writeFileSync } from 'fs';
 export { readFileSync } from 'node:fs';
 
-export function sayHello(name: string) {
+export function sayHello(name: SomeType) {
 	return `Hello ${name}!`;
 }

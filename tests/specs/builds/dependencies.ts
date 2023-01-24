adf1584b9da97a76e4cc7279f1685904be7bc9c6
hiroki osame <hiroki.osame@gmail.com>
2023-01-24 11:21:22 -0500

    fix: externalize @types/ (#29)

--- tests/specs/builds/dependencies.ts
+++ tests/specs/builds/dependencies.ts
@@ -1,6 +1,6 @@
 import { testSuite, expect } from 'manten';
 import { createFixture } from 'fs-fixture';
-import { pkgroll } from '../../utils';
+import { pkgroll, installTypeScript } from '../../utils';
 
 export default testSuite(({ describe }, nodePath: string) => {
 	describe('dependencies', ({ test }) => {
@@ -21,6 +21,40 @@ export default testSuite(({ describe }, nodePath: string) => {
 
 			const content = await fixture.readFile('dist/dependency-external.js', 'utf8');
 			expect(content).toMatch('require(\'@org/name/path\')');
+
+			await fixture.rm();
+		});
+
+		test('externalize types', async () => {
+			const fixture = await createFixture({
+				'package.json': JSON.stringify({
+					types: 'dist/index.d.ts',
+					dependencies: {
+						'@types/pkg': '*',
+						'@types/square-icons__react': '*',
+					},
+				}),
+				'node_modules/@types': {
+					'pkg/index.d.ts': 'export type typeA = {}',
+					'square-icons__react/index.d.ts': 'export type typeB = {}',
+				},
+				'src/index.d.ts': `
+				import type { typeA } from 'pkg';
+				import type { typeB } from '@square-icons/react';
+				export const a: typeA;
+				export const b: typeB;
+				`,
+			});
+
+			installTypeScript(fixture.path);
+
+			const pkgrollProcess = await pkgroll([], { cwd: fixture.path, nodePath });
+			expect(pkgrollProcess.exitCode).toBe(0);
+			expect(pkgrollProcess.stderr).toBe('');
+
+			const content = await fixture.readFile('dist/index.d.ts', 'utf8');
+			expect(content).toMatch('from \'pkg\'');
+			expect(content).toMatch('from \'@square-icons/react\'');
 
 			await fixture.rm();
 		});

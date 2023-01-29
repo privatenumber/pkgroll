67ac25fc97dc7e58fc60a61dbe00a52708cf6f77
Hiroki Osame <osame@squareup.com>
2023-01-29 09:12:26 +0900

    test: make sure jsx can be resolved from js

--- tests/specs/builds/typescript.ts
+++ tests/specs/builds/typescript.ts
@@ -7,8 +7,31 @@ export default testSuite(({ describe }, nodePath: string) => {
 		test('resolves .jsx -> .tsx', async () => {
 			const fixture = await createFixture({
 				src: {
-					'index.ts': 'import "./foo.jsx"',
-					'foo.tsx': 'console.log(1)',
+					'index.ts': 'import "./file.jsx"',
+					'file.tsx': 'console.log(1)',
+				},
+				'package.json': JSON.stringify({
+					main: './dist/index.js',
+					type: 'module',
+				}),
+			});
+
+			const pkgrollProcess = await pkgroll(['--env.NODE_ENV=development'], { cwd: fixture.path, nodePath });
+
+			expect(pkgrollProcess.exitCode).toBe(0);
+			expect(pkgrollProcess.stderr).toBe('');
+
+			const content = await fixture.readFile('dist/index.js', 'utf8');
+			expect(content).toBe('console.log(1);\n');
+
+			await fixture.rm();
+		});
+
+		test('resolves .jsx from .js', async () => {
+			const fixture = await createFixture({
+				src: {
+					'index.ts': 'import "./file.js"',
+					'file.jsx': 'console.log(1)',
 				},
 				'package.json': JSON.stringify({
 					main: './dist/index.js',

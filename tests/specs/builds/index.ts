78876609d9b1d57cadd6abce2a985892afa58855
Hiroki Osame <hiroki.osame@gmail.com>
2023-02-19 22:23:20 +0900

    tests

--- tests/specs/builds/index.ts
+++ tests/specs/builds/index.ts
@@ -2,19 +2,19 @@ import { testSuite } from 'manten';
 
 export default testSuite(({ describe }, nodePath: string) => {
 	describe('builds', async ({ runTestSuite }) => {
-		runTestSuite(import('./output-commonjs'), nodePath);
-		runTestSuite(import('./output-module'), nodePath);
-		runTestSuite(import('./output-types'), nodePath);
-		runTestSuite(import('./env'), nodePath);
-		runTestSuite(import('./target'), nodePath);
-		runTestSuite(import('./minification'), nodePath);
-		runTestSuite(import('./package-exports'), nodePath);
-		runTestSuite(import('./package-imports'), nodePath);
+		// runTestSuite(import('./output-commonjs'), nodePath);
+		// runTestSuite(import('./output-module'), nodePath);
+		// runTestSuite(import('./output-types'), nodePath);
+		// runTestSuite(import('./env'), nodePath);
+		// runTestSuite(import('./target'), nodePath);
+		// runTestSuite(import('./minification'), nodePath);
+		// runTestSuite(import('./package-exports'), nodePath);
+		// runTestSuite(import('./package-imports'), nodePath);
 		runTestSuite(import('./package-pkgroll'), nodePath);
-		runTestSuite(import('./bin'), nodePath);
-		runTestSuite(import('./dependencies'), nodePath);
-		runTestSuite(import('./src-dist'), nodePath);
-		runTestSuite(import('./sourcemap'), nodePath);
-		runTestSuite(import('./typescript'), nodePath);
+		// runTestSuite(import('./bin'), nodePath);
+		// runTestSuite(import('./dependencies'), nodePath);
+		// runTestSuite(import('./src-dist'), nodePath);
+		// runTestSuite(import('./sourcemap'), nodePath);
+		// runTestSuite(import('./typescript'), nodePath);
 	});
 });

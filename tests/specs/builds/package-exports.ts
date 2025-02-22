7f4738632a825d922654a4b5e8a0a42368db2774
Hiroki Osame <hiroki.osame@gmail.com>
2025-02-22 21:34:01 +0900

    lintfix

--- tests/specs/builds/package-exports.ts
+++ tests/specs/builds/package-exports.ts
@@ -124,7 +124,7 @@ export default testSuite(({ describe }, nodePath: string) => {
 					},
 				}),
 			});
-			
+
 			const pkgrollProcess = await pkgroll([], {
 				cwd: fixture.path,
 				nodePath,

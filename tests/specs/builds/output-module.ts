c6e4cda526984bb1a1afd7c1d0145a97375dff0f
Hiroki Osame <hiroki.osame@gmail.com>
2025-02-26 14:36:21 +0900

    fix: dont inject createRequire on object property

--- tests/specs/builds/output-module.ts
+++ tests/specs/builds/output-module.ts
@@ -276,7 +276,29 @@ export default testSuite(({ describe }, nodePath: string) => {
 
 			test('defined require should not get a createRequire', async () => {
 				await using fixture = await createFixture({
-					'src/dynamic-require.ts': 'const require = ()=>{}; require((() => \'fs\')());',
+					'src/dynamic-require.ts': 'const require=console.log; require((() => \'fs\')());',
+					'package.json': createPackageJson({
+						main: './dist/dynamic-require.mjs',
+					}),
+				});
+
+				const pkgrollProcess = await pkgroll([], {
+					cwd: fixture.path,
+					nodePath,
+				});
+
+				expect(pkgrollProcess.exitCode).toBe(0);
+				expect(pkgrollProcess.stderr).toBe('');
+
+				const content = await fixture.readFile('dist/dynamic-require.mjs', 'utf8');
+				expect(content).not.toMatch('createRequire');
+				expect(content).not.toMatch('(import.meta.url)');
+				expect(content).toMatch('"fs"');
+			});
+
+			test('object property should not get a createRequire', async () => {
+				await using fixture = await createFixture({
+					'src/dynamic-require.ts': 'console.log({ require: 1 });',
 					'package.json': createPackageJson({
 						main: './dist/dynamic-require.mjs',
 					}),

8071dd8616fd67f4809df7d6b0fbab921eeba2c4
Charlie Croom <ccroom@squareup.com>
2025-04-24 11:52:59 -0400

    fix: improve error messages when files are missing or mismatched

--- tests/specs/error-cases.ts
+++ tests/specs/error-cases.ts
@@ -106,5 +106,50 @@ export default testSuite(({ describe }, nodePath: string) => {
 			expect(pkgrollProcess.stderr).toMatch('Ignoring entry outside of ./dist/ directory: package.json#main="/dist/main.js"');
 			expect(pkgrollProcess.stderr).toMatch('No export entries found in package.json');
 		});
+
+		test('cannot find matching source file', async () => {
+			await using fixture = await createFixture({
+				...packageFixture(),
+				'package.json': createPackageJson({
+					name: 'pkg',
+					main: 'dist/missing.js',
+					module: 'dist/missing.mjs',
+				}),
+			});
+
+			const pkgrollProcess = await pkgroll(
+				[],
+				{
+					cwd: fixture.path,
+					nodePath,
+					reject: false,
+				},
+			);
+			expect(pkgrollProcess.exitCode).toBe(1);
+			expect(pkgrollProcess.stderr).toMatch('Could not find matching source file for export path');
+			expect(pkgrollProcess.stderr).toMatch('Expected: ./src/missing[.js|.ts|.tsx|.mts|.cts]');
+		});
+
+		test('unexpected extension', async () => {
+			await using fixture = await createFixture({
+				...packageFixture(),
+				'package.json': createPackageJson({
+					name: 'pkg',
+					main: 'dist/index.foo',
+				}),
+			});
+
+			const pkgrollProcess = await pkgroll(
+				[],
+				{
+					cwd: fixture.path,
+					nodePath,
+					reject: false,
+				},
+			);
+			expect(pkgrollProcess.exitCode).toBe(1);
+			expect(pkgrollProcess.stderr).toMatch('Error: Package.json output path contains invalid extension');
+			expect(pkgrollProcess.stderr).toMatch('Expected: .d.ts, .d.mts, .d.cts, .js, .mjs, .cjs');
+		});
 	});
 });

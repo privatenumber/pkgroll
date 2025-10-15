46106b834dacc8ceb133c54c3ae8688b20d3c2b0
Hiroki Osame <hiroki.osame@gmail.com>
2025-10-15 16:33:29 +0900

    wip

--- src/utils/read-package.ts
+++ src/utils/read-package.ts
@@ -1,43 +1,53 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import path from 'node:path';
 import type { PackageJson } from 'type-fest';
 import { parse as yamlParse } from 'yaml';
 import { fsExists } from './fs-exists.js';
 import { formatPath } from './log.js';
 
+const readPackageYaml = async (
+	packageYamlPath: string,
+): Promise<PackageJson> => {
+	const packageYamlString = await fs.readFile(packageYamlPath, 'utf8');
+
+	try {
+		return yamlParse(packageYamlString);
+	} catch (error) {
+		throw new Error(`Failed to parse ${formatPath(packageYamlPath)}: ${(error as Error).message}`);
+	}
+};
+
+const readPackageJson = async (
+	packageJsonPath: string,
+): Promise<PackageJson> => {
+	const packageJsonString = await fs.readFile(packageJsonPath, 'utf8');
+
+	try {
+		return JSON.parse(packageJsonString);
+	} catch (error) {
+		throw new Error(`Failed to parse ${formatPath(packageJsonPath)}: ${(error as Error).message}`);
+	}
+};
+
 export const readPackage = async (directoryPath: string) => {
 	const packageYamlPath = path.join(directoryPath, 'package.yaml');
-	const packageJsonPath = path.join(directoryPath, 'package.json');
-
-	let packagePath: string;
-	let packageObject: PackageJson;
 
 	if (await fsExists(packageYamlPath)) {
-		packagePath = packageYamlPath;
-
-		const packageYamlString = await fs.promises.readFile(packageYamlPath, 'utf8');
-
-		try {
-			packageObject = yamlParse(packageYamlString);
-		} catch (error) {
-			throw new Error(`Failed to parse ${formatPath(packageYamlPath)}: ${(error as Error).message}`);
-		}
-	} else if (await fsExists(packageJsonPath)) {
-		packagePath = packageJsonPath;
-
-		const packageJsonString = await fs.promises.readFile(packageJsonPath, 'utf8');
-
-		try {
-			packageObject = JSON.parse(packageJsonString);
-		} catch (error) {
-			throw new Error(`Failed to parse ${formatPath(packageJsonPath)}: ${(error as Error).message}`);
-		}
-	} else {
-		throw new Error(`package.json not found at: ${packageJsonPath} (also checked for package.yaml)`);
+		const packageJson = await readPackageYaml(packageYamlPath);
+		return {
+			packageJson,
+			packageJsonPath: packageYamlPath,
+		};
 	}
 
-	return {
-		packageJson: packageObject,
-		packageJsonPath: packagePath,
-	};
+	const packageJsonPath = path.join(directoryPath, 'package.json');
+	if (await fsExists(packageJsonPath)) {
+		const packageJson = await readPackageJson(packageJsonPath);
+		return {
+			packageJson,
+			packageJsonPath,
+		};
+	}
+
+	throw new Error(`package.json not found at: ${packageJsonPath} (also checked for package.yaml)`);
 };
